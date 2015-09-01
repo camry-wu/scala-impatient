6281d345226f5fe3b07f8e3b44dff80db96a4fbc
wuhao <wuhao@fortinet.com>
2015-09-01 19:47:28 +0800

    init

---
+++ src/main/scala/sec1/prac1.scala
@@ -0,0 +1,42 @@
+
+// 1.
+// scala REPL has no feedback while print TAB
+
+// 2.
+// import scala.math._
+// pow(sqrt(3), 2) = 2.9999...6
+
+// 3.
+// res is val
+
+// 4.
+// "crazy" * 3 = crazycrazycrazy
+
+// 5.
+// 10 max 2 = 10.max(2) = 10
+// scala.math.max
+
+// 6.
+// val a = BigInt(2)
+// val b = a.pow(1024)
+
+// 7. 
+// import scala.math.BigInt._
+// probablePrime(100, scala.util.Random)
+
+// 8.
+// val b = probablePrime(100, scala.util.Random)
+// val c = b.toString(36)
+
+// 9.
+// "crazy".head or "crazy"(0)
+// "crazy".last or "crazy"("crazy".length - 1)
+
+// 10.
+// "camry".take(3) = "cam"          "camry".substring(0, 3)
+// "camry".takeRight(3) = "mry"     "camry".substring("camry".length - 3)
+// "camry".drop(3) = "ry"           "camry".substring(3)
+// "camry".dropRight(3) = "ca"      "camry".substring(0, "camry".length - 3)
+
+// good: more readable, more security(donot throw IndexOutOfBound error)
+// bad: no, maybe low performance? or donot throw error
---
+++ src/main/scala/sec2/prac1.scala
@@ -0,0 +1,115 @@
+package sec2
+
+class Prac1 {
+}
+
+object Prac1 extends App {
+    println ("sec2.Prac1")
+
+    // 1.
+    println("------------------------------  practice 1 -------------------------");
+    def getSignNum(a: Int): Int = {
+        val sign =
+            if (a > 0) 1 
+            else if (a == 0) 0
+            else -1
+
+        sign
+    }
+
+    println ("getSignNum(5) = " + getSignNum(5))
+    println ("getSignNum(0) = " + getSignNum(0))
+    println ("getSignNum(-8) = " + getSignNum(-8))
+
+    // 2. {} value is Unit, type is Unit ?
+
+    // 3.
+    // var x: Unit = Unit
+    // var y: Int = 0
+    // x = y = 1            // then x = Unit, y = 1
+
+    // 4.
+    println("------------------------------  practice 4 -------------------------");
+    for (a <- (1 to 10).reverse) println (a)
+
+    // 5.
+    println("------------------------------  practice 5 -------------------------");
+    def countdown(n: Int) {
+        if (n > 0)
+            for (a <- (0 to n).reverse) println (a)
+        else
+            println ("Exception: n <= 0");
+    }
+
+    countdown(5);
+    countdown(-1);
+
+    // 6.
+    println("------------------------------  practice 6 -------------------------");
+    def letterProduct(str: String) : BigInt = {
+        val opt: Option[String] = Option(str)
+
+        var sum = BigInt(1)
+
+        opt match {
+            case Some(s) => for (a <- s) sum *= a.toInt
+            case None => println ("Exception: null string!")
+        }
+        sum
+    }
+
+    println (letterProduct("Hello"))
+    println (letterProduct(null))
+
+    // 7.
+    // 8.
+    println("------------------------------  practice 7, 8 -------------------------");
+    def product(str: String) : BigInt = {
+        val opt: Option[String] = Option(str)
+
+        var sum = BigInt(1)
+
+        opt match {
+            case Some(s) => sum = s.toBuffer[Char].map(_.toInt).map(BigInt(_)).product
+            case None => println ("Exception: null string!")
+        }
+        sum
+    }
+
+    println (product("Hello"))
+
+    // 9.
+    println("------------------------------  practice 9 -------------------------");
+    def product2(str: String) : BigInt = {
+        val opt: Option[String] = Option(str)
+
+        var sum: BigInt = null
+
+        opt match {
+            case Some(s) => sum = if(s.isEmpty) 0 else recursiveProductStr(BigInt(1), s)
+            case None => println ("Exception: null string!")
+        }
+        sum
+    }
+
+    def recursiveProductStr(prod: BigInt, str: String): BigInt = {
+        if(str.isEmpty()) prod
+        else recursiveProductStr(prod * BigInt(str.head.toInt), str.tail)
+    }
+
+    println (product2("Hello"))
+
+    // 10.
+    println("------------------------------  practice 10 -------------------------");
+    def mypow(x: BigInt, n: Int) : BigInt = {
+        if (n < 0) 1 / mypow(x, -1 * n)
+        else if (n == 0) 1
+        else if (n % 2 == 0) mypow(x, n / 2) * mypow(x, n / 2)
+        else x * mypow(x, n - 1)
+    }
+
+    println (mypow(2, 3))
+    println (mypow(3, 3))
+    println (mypow(2, 1024))
+    println (BigInt(2)pow(1024))
+}
