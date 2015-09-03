f79b2c675b5e980e37f91bb7b85cd682c985b3c0
camry.wu <camry.wu@gmail.com>
2015-09-03 08:08:42 +0800

    finish sec4

--- src/main/scala/sec4/prac1.scala
+++ src/main/scala/sec4/prac1.scala
@@ -34,7 +34,18 @@ object Prac1 extends App {
         res
     }
 
-    val source = Source.fromFile("/tmp/A.java", "UTF-8")
+    // create file
+    val filename = "./LICENSE"
+    val file = new java.io.File(filename)
+    if (!file.exists()) {
+        val out = new java.io.PrintWriter(file)
+        for (i <- 1 to 100) {
+            out.println(i)
+        }
+        out.close()
+    }
+
+    val source = Source.fromFile(filename, "UTF-8")
     val in = source.mkString.split("""[\s;$/:~\[\]\(\)\{\}-]+""")  // =, *, @ cannot set?
 
     val wc = wordCount(in)
@@ -106,17 +117,68 @@ object Prac1 extends App {
 
     // 6.
     println("------------------------------  practice 6 -------------------------");
+    import java.util.Calendar
+    val lm = new LinkedHashMap[String, Int]
+    lm("Monday") = Calendar.MONDAY
+    lm("Tuesday") = Calendar.TUESDAY
+    lm("Wednesday") = Calendar.WEDNESDAY
+    lm("Thursday") = Calendar.THURSDAY
+    lm("Friday") = Calendar.FRIDAY
+    lm("Saturday") = Calendar.SATURDAY
+    lm("Sunday") = Calendar.SUNDAY
+    println(lm.mkString("\n"))
 
     // 7.
     println("------------------------------  practice 7 -------------------------");
     import java.util.Properties
+    import scala.collection.JavaConversions.propertiesAsScalaMap
+    val props: scala.collection.Map[String, String] = System.getProperties()
+    val key_length: Int = props.keySet.map(_.length).reduceLeft(math.max(_, _)) + 1
+
+    for((k, v) <- props) {
+        print(k)
+        print(" " * (key_length - k.length))
+        print("| ")
+        println(v)
+    }
 
     // 8.
     println("------------------------------  practice 8 -------------------------");
+    def minmax(values: Array[Int]): Tuple2[Int, Int] = {
+        var min: Int = Integer.MAX_VALUE
+        var max: Int = Integer.MIN_VALUE
+
+        for (elem <- values) {
+            min = math.min(min, elem)
+            max = math.max(max, elem)
+        }
+        
+        (min, max)
+    }
+
+    println(minmax(Array(2, 3, 4, 1, 5, 9, 6, 7, 8)))
 
     // 9.
     println("------------------------------  practice 9 -------------------------");
+    def lteqgt(values: Array[Int], v: Int): Tuple3[Int, Int, Int] = {
+        var lt: Int = 0
+        var eq: Int = 0
+        var gt: Int = 0
 
-    // 10.
+        for (elem <- values) {
+            (elem compare v) match {
+                case 1 => gt += 1
+                case 0 => eq += 1
+                case -1 => lt += 1
+            }
+        }
+        
+        (lt, eq, gt)
+    }
+
+    println(lteqgt(Array(2, 3, 4, 1, 5, 9, 6, 7, 8), 5))
+
+    // 10.  Vector((H, W), (e, o), (l, r), (l, l), (o, d))
     println("------------------------------  practice 10 -------------------------");
+    println("Hello".zip("World"))
 }
