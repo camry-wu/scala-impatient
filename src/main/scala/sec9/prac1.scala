de9a845fe2f8b861e70d8e08cf4c73fcdb7e842c
wuhao <wuhao@fortinet.com>
2015-09-18 19:13:33 +0800

    update

--- src/main/scala/sec9/prac1.scala
+++ src/main/scala/sec9/prac1.scala
@@ -39,6 +39,22 @@ object InvertFile {
 // 7.
 
 // 8.
+import javax.net.ssl._
+import java.security.cert.X509Certificate
+
+// bypasses both client and server validation
+object TrustAll extends X509TrustManager {
+    val getAcceptedIssuers = null
+
+    def checkClientTrusted(x509Certificates: Array[X509Certificate], s: String) = {}
+
+    def checkServerTrusted(x509Certificates: Array[X509Certificate], s: String) = {}
+}
+
+// verifies all host names by simply returning true
+object VerifiesAllHostNames extends HostnameVerifier {
+    def verify(s: String, sslSession: SSLSession) = true
+}
 
 // 9.
 
@@ -73,7 +89,7 @@ object PracTest extends App {
     println("------------------------------  practice 3 -------------------------");
     def findMoreThan12Word(filename: String) {
         val source = Source.fromFile(filename)
-        source.mkString.split("[\\s\\._()\"{}]+").filter(_.length > 12).foreach(println(_))
+        source.mkString.split("[\\s\\._\"(\")\"{}]+").filter(_.length > 12).foreach(println(_))
         source.close
     }
     findMoreThan12Word("./s/sec1/prac1.scala")
@@ -117,17 +133,58 @@ object PracTest extends App {
 
     // 6.
     println("------------------------------  practice 6 -------------------------");
+    def prac6(filename: String) {
+        val source = Source.fromFile(filename)
+        val strPattern = """("([^"]*(?:\\")+[^"]*)")""".r
+        strPattern.findAllIn(source.mkString).foreach(println(_))
+        source.close
+    }
+    prac6("./s/sec9/test6.txt")
 
     // 7.
     println("------------------------------  practice 7 -------------------------");
     def prac7(filename: String) {
+        val source = Source.fromFile(filename)
+        val numberPattern = """(?i:\d+\.\d+)|(\d+)""".r
+        val tokens = numberPattern.split(source.mkString).flatMap(_.split("[/;<>\\[\\]\\-\\s\\._()\"{}]+"))
+        //val tokens = numberPattern.split(source.mkString)
+        for (tk <- tokens) {
+            println(tk)
+        }
+        source.close
     }
+    //prac7("./s/sec9/prac1.scala")
 
     // 8.
     println("------------------------------  practice 8 -------------------------");
+    def prac8(url: String) {
+        // SSL Context initialization and configuration
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, Array(TrustAll), new java.security.SecureRandom())
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory)
+        HttpsURLConnection.setDefaultHostnameVerifier(VerifiesAllHostNames)
+
+        val numberPattern = """(?i:\d+\.\d+)|(\d+)""".r
+        val source = Source.fromURL(url)
+        source.mkString
+        source.close
+    }
+    prac8("https://192.168.1.101/semanto/")
 
     // 9.
     println("------------------------------  practice 9 -------------------------");
+    import java.io.File
+    def subdirs(dir: File): Iterator[File] = {
+        val children = dir.listFiles.filter(_.isDirectory)
+        children.toIterator ++ children.toIterator.flatMap(subdirs _)
+    }
+    /*
+    for (d <- subdirs(new File("./target/"))) {
+        val files = d.listFiles.filter(!_.isDirectory).toIterator
+        for(f <- files if f.getName.endsWith(".class")) {
+            println(f.getCanonicalPath)
+        }
+    }*/
 
     // 10.
     println("------------------------------  practice 10 -------------------------");
