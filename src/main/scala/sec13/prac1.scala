c1c998e006e6d263d018e0e7257e5fb9f67a3c6d
camry.wu <camry.wu@gmail.com>
2015-10-18 22:46:05 +0800

    update sec13

--- src/main/scala/sec13/prac1.scala
+++ src/main/scala/sec13/prac1.scala
@@ -6,6 +6,7 @@ import scala.collection.Set
 import scala.collection.immutable.List
 import scala.collection.mutable.LinkedHashSet
 import scala.collection.mutable.{HashMap => MHashMap}
+import scala.collection.mutable.SynchronizedMap
 
 // 2.
 
@@ -20,6 +21,7 @@ import scala.collection.mutable.{HashMap => MHashMap}
 // 7.
 
 // 8.
+import scala.collection.mutable.ArrayBuffer
 
 // 9.
 
@@ -115,14 +117,56 @@ object PracTest extends App {
     val lst2 = (List[Int]() /: lst)(_ :+ _)
     println(lst2)       // result: List(1, 2, 3, 4, 5)
 
+    val lst3 = (lst :\ List[Int]())((x: Int, y: List[Int]) => y :+ x)
+    println(lst3)       // result: List(5, 4, 3, 2, 1)
+
     // 7.
     println("------------------------------  practice 7 -------------------------");
+    val prices = 1 to 5
+    val quantities = 11 to 15
+    val pq = prices zip quantities
+    println(pq map { p => p._1 * p._2 })
+    println(pq.map(Function.tupled[Int, Int, Int]((x, y) => x * y)(_)))
+    println(pq.map(Function.tupled[Int, Int, Int](_ * _)(_)))
+    // println(pq.map(Function.tupled(_ * _)(_)))   // compile error
 
     // 8.
     println("------------------------------  practice 8 -------------------------");
+    def to2Array(arr: Array[Int], col: Int): Array[Array[Int]] = {
+        val result = new ArrayBuffer[Array[Int]]()
+        val x = arr.grouped(col)
+        for (a <- x) {
+            result += a
+        }
+        result.toArray
+    }
+
+    val arr = Array(1, 2, 3, 4, 5, 6)
+    val brr = to2Array(arr, 3)
+    for (a <- brr) {
+        print("[")
+        print(a.mkString(","))
+        println("], ")
+    }
 
     // 9.
     println("------------------------------  practice 9 -------------------------");
+    // val frequencies = new MHashMap[Char, Int] with SynchronizedMap[Char, Int]
+    // new Thread() {
+    //     for (get c from file) {
+    //         frequencies(c) = frequencies.getOrElse(c, 0) + 1
+    //     }
+    // }.start()
+    // 
+    // 注意：两个线程并发访问 getOrElse 的时候可能是得到不同的数字
+    // 再继续执行增加的方法，就可能得到错误的结果
+    // 如：thead a 读到 1, 应该设置为 2
+    // 而  thead b 也读到 1, 也会设置为 2
+    // 最后结果设置为 2, 但实际上应该是 3 了
+
+    import scala.collection.JavaConversions.asScalaConcurrentMap
+    // val frequencies: scala.collection.mutable.ConcurrentMap[Char, Int] =
+    //      new java.util.concurrent.ConcurrentHashMap[Char, Int]
 
     // 10.
     println("------------------------------  practice 10 -------------------------");
